88bb12b315079393d28043916087e39574aa7877
aniruddha <aniruddha.kudalkar@gmail.com>
2017-02-13 12:16:07 +0530

    animations

---
+++ animations/src/main/java/com/codekul/animations/MainActivity.java
@@ -0,0 +1,22 @@
+package com.codekul.animations;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnAnimate).setOnClickListener(this::animate);
+    }
+
+    private void animate(View view) {
+        findViewById(R.id.imgVeh)
+                .startAnimation(AnimationUtils.loadAnimation(this, R.anim.rozo));
+    }
+}
