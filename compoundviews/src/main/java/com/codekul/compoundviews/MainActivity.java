0fd32ec142127f316843c9bb4b8f091cfb81535e
aniruddha <aniruddha.kudalkar@gmail.com>
2017-02-13 12:47:59 +0530

    Layout inflater

---
+++ compoundviews/src/main/java/com/codekul/compoundviews/MainActivity.java
@@ -0,0 +1,25 @@
+package com.codekul.compoundviews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RelativeLayout layRt = (RelativeLayout) findViewById(R.id.layRt);
+
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+
+        View compoundView = inflater.inflate(R.layout.compound_view, layRt, true);
+        ((TextView)compoundView.findViewById(R.id.txtInf)).setText("This is new info");
+
+    }
+}
